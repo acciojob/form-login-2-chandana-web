30de3c5756fd0e55be335be2212f466957035715
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-17 00:07:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-//your JS code here. If required.
+const c=document.querySelector(".info");
+     c.addEventListener("submit", function(e){
+		e.preventDefault();
+		var fname=document.getElementById("fname").value;
+		var lname=document.getElementById("lname").value;
+		var phone=document.getElementById("phone").value;
+		var email=document.getElementById("email").value;
+		 alert('First Name: ' + fname + '\nLast Name: ' + lname + '\nPhone Number: ' + phone + '\nEmail: ' + email);
+	});
