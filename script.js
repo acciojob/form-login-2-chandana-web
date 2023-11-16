8793061ba38d88f20dcc5140167428a204f1ba5c
chandana-web <143087553+chandana-web@users.noreply.github.com>
2023-11-17 00:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,5 +5,5 @@ const c=document.querySelector(".info");
 		var lname=document.getElementById("lname").value;
 		var phone=document.getElementById("phone").value;
 		var email=document.getElementById("email").value;
-		 window.alert('First Name: ' + fname + '\nLast Name: ' + lname + '\nPhone Number: ' + phone + '\nEmail ID: ' + email);
+		alert('First Name: ' + fname + '\nLast Name: ' + lname + '\nPhone Number: ' + phone + '\nEmail ID: ' + email);
 	});
